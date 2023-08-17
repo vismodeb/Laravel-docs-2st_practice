cff65d0ac2aae9eee0ef4f6d66fd7fc914a8a2a0
vismo_dev <vismodeb2000@gmail.com>
2023-08-17 11:28:29 +0600

    First Laravel admin agent user login Create

---
+++ resources/views/agent/agent_dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agent Deshboard</title>
+</head>
+<body>
+    <h2>Agent Deshboard</h2>
+</body>
+</html>
