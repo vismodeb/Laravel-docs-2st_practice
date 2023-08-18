1c98a0e939be9eb927bfa20585c2f7451fd269d4
vismo_dev <vismodeb2000@gmail.com>
2023-08-18 21:47:12 +0600

    Laravel Custom Project Ready

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>Laravel Pektij Design</title>
 </head>
+
 <body>
-    <h2>Admin Deshboard</h2>
+
+    <!-- Sidebar -->
+    @include('admin.body.sidebar')
+    <!-- End of Sidebar -->
+
+    <!-- Main Content -->
+    <div class="content">
+        <!-- Navbar -->
+        @include('admin.body.header')
+
+        <!-- End of Navbar -->
+
+        @yield('admin')
+
+    </div>
+
+    <script src="{{ asset('assets/js/index.js') }}"></script>
 </body>
+
 </html>
